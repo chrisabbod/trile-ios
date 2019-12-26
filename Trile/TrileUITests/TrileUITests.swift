63a9cc4d3678ffadd226b72051b71e252fb5c3b4
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-12-26 17:33:05 -0500

    Add UITest testAddClientAlertDialogCancelButtonCancelsDialog

--- Trile/TrileUITests/TrileUITests.swift
+++ Trile/TrileUITests/TrileUITests.swift
@@ -10,34 +10,28 @@ import XCTest
 
 class TrileUITests: XCTestCase {
 
+    var app: XCUIApplication!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
+    
+    func testAddClientAlertDialogCancelButtonCancelsDialog() {
+        //given
+        let addClientButton = app.navigationBars["Master"].buttons["Add"]
+        let alertDialog = app.alerts["Add New Client"]
+        let cancelButton = app.alerts["Add New Client"].scrollViews.otherElements.buttons["Cancel"]
 
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+        //when
+        addClientButton.tap()
+        cancelButton.tap()
+        
+        //then
+        XCTAssertTrue(!alertDialog.exists)
     }
 }
