2e3308e9501bff6e9ee72cbf353194c1b849e2ba
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2020-01-01 17:58:51 -0500

    Build Client Detail Screen

---
+++ Trile/Trile/Controller/ClientDetailsVC.swift
@@ -0,0 +1,31 @@
+//
+//  ClientDetailsVC.swift
+//  Trile
+//
+//  Created by Chris Abbod on 1/1/20.
+//  Copyright © 2020 Trile. All rights reserved.
+//
+
+import UIKit
+
+class ClientDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        circularImage.layer.masksToBounds = true
+//        circularImage.layer.cornerRadius = circularImage.bounds.width / 2
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
