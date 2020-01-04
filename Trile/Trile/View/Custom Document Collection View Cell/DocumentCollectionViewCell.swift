b26c6b42433b231479af67ec306f9eeaa50c48dc
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2020-01-03 21:13:41 -0500

    Create custom Document Collection View Cell class and XIB files

---
+++ Trile/Trile/View/Custom Document Collection View Cell/DocumentCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DocumentCollectionViewCell.swift
+//  Trile
+//
+//  Created by Chris Abbod on 1/3/20.
+//  Copyright © 2020 Trile. All rights reserved.
+//
+
+import UIKit
+
+class DocumentCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var documentImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
