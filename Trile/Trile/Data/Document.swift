298944aedff5bc485877cb6f38d9ba5fe5566c0e
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2020-01-03 19:14:11 -0500

    Add Document.swift class

---
+++ Trile/Trile/Data/Document.swift
@@ -0,0 +1,13 @@
+//
+//  Document.swift
+//  Trile
+//
+//  Created by Chris Abbod on 1/3/20.
+//  Copyright © 2020 Trile. All rights reserved.
+//
+
+import Foundation
+
+class Document {
+    var documentID: String = ""
+}
