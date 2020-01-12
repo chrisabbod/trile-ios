332eb76a4a8e5724e6af3f33056b5bc0b5af78b0
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2020-01-11 22:55:29 -0500

    FeeApplicationVC now shows an editable Fee Application PDF

--- Trile/Trile/Controller/FeeApplicationVC.swift
+++ Trile/Trile/Controller/FeeApplicationVC.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import PDFKit
 
 class FeeApplicationVC: UIViewController {
+    
+    let fileURL: URL = URL(string: "https://firebasestorage.googleapis.com/v0/b/trile-6bbdc.appspot.com/o/Fee%20Application%2FFee%20Application.pdf?alt=media&token=f4702080-ce8f-4827-9011-8e7e92d20213")!
     
     var selectedClient: Client?
     var selectedFileNumber: FileNumber?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("Client: \(selectedClient?.name)")
-        print("File Number: \(selectedFileNumber?.assignedFileNumber)")
+        
+        showPDF()
     }
     
+    func showPDF() {
+        let pdfView = PDFView()
+
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+
+        pdfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        pdfView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        if let document = PDFDocument(url: fileURL) {
+            pdfView.document = document
+        }
+    }
 }
