612cb6b776832138dfbc87bc12e5b3c63f349621
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2020-01-25 18:27:25 -0500

    Add text restrictions to travel, copying, and other expenses in EditCaseDetailsVC

--- Trile/Trile/Controller/EditCaseDetailsVC.swift
+++ Trile/Trile/Controller/EditCaseDetailsVC.swift
@@ -166,6 +166,8 @@ class EditCaseDetailsVC: UIViewController, UITextFieldDelegate, UIPickerViewDele
             return TextRestrictionManager.restrictTextLength(by: 20, textField, shouldChangeCharactersIn: range, replacementString: string)
         } else if textField == bondTextField {
             return TextRestrictionManager.restrictTextLengthAndCharacters(by: 10, textField, shouldChangeCharactersIn: range, replacementString: string)
+        } else if textField == travelExpensesTextField || textField == copyingExpensesTextField || textField == otherExpensesTextField {
+            return TextRestrictionManager.restrictTextLengthAndCharacters(by: 8, textField, shouldChangeCharactersIn: range, replacementString: string)
         } else if textField == sentenceMonthsTextField {
             return TextRestrictionManager.restrictTextLengthAndCharacters(by: 4, textField, shouldChangeCharactersIn: range, replacementString: string)
         } else if textField == continuancesTextField || textField == sentenceDaysTextField || textField == timeInCourtHoursTextField || textField == timeInCourtMinutesTextField || textField == timeInCourtWaitingHoursTextField || textField == timeInCourtWaitingMinutesTextField || textField == timeOutOfCourtHoursTextField || textField == timeOutOfCourtMinutesTextField {
