e317b1dac1d543d23cb0c3e5771ee4ff1e05274e
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2020-01-26 22:40:42 -0500

    Move account creation back to AccountCreationVC

--- Trile/Trile/Controller/Onboarding/AccountCreationVC.swift
+++ Trile/Trile/Controller/Onboarding/AccountCreationVC.swift
@@ -11,7 +11,7 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class AccountCreationVC: UIViewController {
-
+    
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -19,59 +19,67 @@ class AccountCreationVC: UIViewController {
     
     let MIN_LENGTH = 6
     
-    let newUser = User()
+    let db = Firestore.firestore()
     let alert = AlertPresenterManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     //MARK: Button Functions
     
-    @IBAction func continueButton(_ sender: Any) {
-        getUserInformation { (success) in
-            if success {
-                self.performSegue(withIdentifier: "showAdditionalInfoVC", sender: self)
+    @IBAction func createAccountButton(_ sender: Any) {
+        createNewUser()
+    }
+    
+    func createNewUser() {
+        
+        let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        var errorMessage: String = ""
+        
+        
+        if firstName.isEmpty {
+            errorMessage = "A first name must be provided"
+            self.alert.errorMessageAlertDialog(fromViewController: self, withMessage: errorMessage)
+        } else if lastName.isEmpty {
+            errorMessage = "A last name must be provided"
+            self.alert.errorMessageAlertDialog(fromViewController: self, withMessage: errorMessage)
+        } else {
+            Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                
+                let uid: String = Auth.auth().currentUser!.uid
+                
+                if error != nil {
+                    if let errorMessage = error?.localizedDescription {
+                        self.alert.errorMessageAlertDialog(fromViewController: self, withMessage: errorMessage)
+                    }
+                    print("Unable to authenticate user \(error.debugDescription)")
+                } else {
+                    
+                    //message = "User was successfully created"
+                    
+                    self.db.collection("users").document(uid).setData([
+                        "first_name":firstName,
+                        "last_name":lastName,
+                        "email": email,
+                        "uid": result!.user.uid])
+                    
+                    self.transitionToHome()
+                }
             }
         }
-    }
-    
-    //MARK: Segue
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destinationVC = segue.destination as! AdditionalInfoVC
-        destinationVC.userData = newUser
-    }
-    
-    func getUserInformation(completion: @escaping ((_ success: Bool) -> Void)) {
-                
-        newUser.firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        newUser.lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        newUser.email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        newUser.password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        let errorMessage: String
-        
-        if newUser.firstName.isEmpty {
-            errorMessage = "No first name entered"
-            alert.errorMessageAlertDialog(fromViewController: self, withMessage: errorMessage)
-            completion(false)
-        } else if newUser.lastName.isEmpty {
-            errorMessage = "No last name entered"
-            alert.errorMessageAlertDialog(fromViewController: self, withMessage: errorMessage)
-            completion(false)
-        } else if newUser.email.isEmpty {
-            errorMessage = "No email entered"
-            alert.errorMessageAlertDialog(fromViewController: self, withMessage: errorMessage)
-            completion(false)
-        } else if newUser.password.isEmpty {
-            errorMessage = "No password entered"
-            alert.errorMessageAlertDialog(fromViewController: self, withMessage: errorMessage)
-            completion(false)
-        } else {
-            completion(true)
-        }
     }
     
+    func transitionToHome() {
+        let loginVC = storyboard?.instantiateViewController(identifier: "LoginViewController")
+        
+        view.window?.rootViewController = loginVC
+        view.window?.makeKeyAndVisible()
+    }
 }
