daae9638f7bfda688388f21d33588cbd50ca117a
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2020-01-26 20:44:57 -0500

    AccountCreationVC only allows user to continue if authentication requirements are all valid

--- Trile/Trile/Controller/Onboarding/AccountCreationVC.swift
+++ Trile/Trile/Controller/Onboarding/AccountCreationVC.swift
@@ -17,13 +17,44 @@ class AccountCreationVC: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    let MIN_LENGTH = 6
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     @IBAction func continueButton(_ sender: Any) {
-        performSegue(withIdentifier: "showAdditionalInfoVC", sender: self)
+        getUserInformation { (success) in
+            if success {
+                self.performSegue(withIdentifier: "showAdditionalInfoVC", sender: self)
+            }
+        }
+    }
+    
+    func getUserInformation(completion: @escaping ((_ success: Bool) -> Void)) {
+        let newUser = User()
+                
+        newUser.firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        newUser.lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        newUser.email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        newUser.password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if newUser.firstName.isEmpty {
+            print("No first name entered")
+            completion(false)
+        } else if newUser.lastName.isEmpty {
+            print("No last name entered")
+            completion(false)
+        } else if newUser.email.isEmpty {
+            print("No email entered")
+            completion(false)
+        } else if newUser.password.isEmpty || newUser.password.count < MIN_LENGTH {
+            print("Invalid password entered")
+            completion(false)
+        } else {
+            completion(true)
+        }
     }
     
 }
---
+++ Trile/Trile/Data/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Trile
+//
+//  Created by Chris Abbod on 1/26/20.
+//  Copyright © 2020 Trile. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    var firstName: String = ""
+    var lastName: String = ""
+    var email: String = ""
+    var password: String = ""
+    var uid: String = ""
+    
+    var firmName: String = ""
+    var taxpayerID: String = ""
+    
+    var address: String = ""
+    var city: String = ""
+    var state: String = ""
+    var zip: String = ""
+}
